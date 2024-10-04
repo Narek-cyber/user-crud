89b206bcbab3efba8f9b275152e9c68b5c78280f
Narek Sahakyan <narsahakyan.work@gmail.com>
2024-10-04 19:18:55 +0400

    Ðadd admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admins = [
+            [
+                'name' => 'John',
+                'email' => 'john@doe.com',
+                'password' => Hash::make('12345678'),
+            ],
+            [
+                'name' => 'Jenna',
+                'email' => 'jenna@doe.com',
+                'password' => Hash::make('12345678'),
+            ],
+        ];
+
+        foreach ($admins as $admin) {
+            User::query()->firstOrCreate(['email' => $admin['email']], $admin);
+        }
+    }
+}
